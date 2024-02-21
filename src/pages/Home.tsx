dea419f9fec8ec47672531b957e2bb4fb613e8d0
golden-state0fmind <areyes2009@yahoo.com>
2024-02-20 19:33:51 -0700

    updates the title on the home screen

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,5 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import ReceiptContainer from '../components/ReceiptContainer';
 import './Home.css';
 
 const Home: React.FC = () => {
@@ -7,16 +7,16 @@ const Home: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>Split Pulse</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
+            <IonTitle size="large">Split Pulse</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer />
+        <ReceiptContainer />
       </IonContent>
     </IonPage>
   );
