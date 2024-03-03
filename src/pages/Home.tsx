d8080f6e944c77f990c291b098cfa4dc8eb9ea2f
golden-state0fmind <areyes2009@yahoo.com>
2024-03-02 19:12:30 -0700

    updates the title to better match the screen

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,13 +7,13 @@ const Home: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Split Pulse</IonTitle>
+          <IonTitle>Upload Receipt</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Split Pulse</IonTitle>
+            <IonTitle size="large">Upload Receipt</IonTitle>
           </IonToolbar>
         </IonHeader>
         <ReceiptContainer />
