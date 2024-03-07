77f7f8f54b47b7fbf373ec6ea3fa69718cee3e20
golden-state0fmind <areyes2009@yahoo.com>
2024-03-07 11:01:39 -0700

    adds new page for users to manually add items from receipt

---
+++ src/pages/ManualEntry.tsx
@@ -0,0 +1,86 @@
+import { IonList, IonItem, IonInput, IonContent, IonGrid, IonHeader, IonPage, IonRow, IonTitle, IonToolbar } from "@ionic/react"
+import { useState } from "react";
+
+const ManualEntry = () => {
+    const [numberOfItems, setNumberOfItems] = useState<number | string>(0);
+    const [priceOfItem, setPriceOfItem] = useState<number | string>(0);
+    const [total, setTotal] = useState<number | string>(0);
+
+    const handleNumberOfItemsChange = (e: CustomEvent) => {
+        const newValue = e.detail.value || '0';
+        setNumberOfItems(newValue);
+        calculateTotal(parseFloat(newValue), parseFloat(priceOfItem as string));
+    };
+
+    const handlePriceOfItemChange = (e: CustomEvent) => {
+        const newValue = e.detail.value || '0';
+        setPriceOfItem(newValue);
+        calculateTotal(parseFloat(numberOfItems as string), parseFloat(newValue));
+    };
+
+    const calculateTotal = (numberOfItems: number, priceOfItem: number) => {
+        const newTotal = numberOfItems * priceOfItem;
+        console.log(newTotal)
+        setTotal(newTotal);
+    };
+
+    return (
+        <IonPage>
+            <IonHeader>
+                <IonToolbar>
+                    <IonTitle>Manually Enter Items</IonTitle>
+                </IonToolbar>
+            </IonHeader>
+            <IonContent fullscreen>
+                <IonHeader collapse="condense">
+                    <IonToolbar>
+                        <IonTitle size="large">Manually Enter Items</IonTitle>
+                    </IonToolbar>
+                </IonHeader>
+                <IonGrid>
+                    <IonRow>
+                        <IonList>
+                            <IonItem>
+                                <IonInput label="Item Name" placeholder="Enter text"></IonInput>
+                            </IonItem>
+
+                            <IonItem>
+                                <IonInput
+                                    label="Number of Item"
+                                    type="number"
+                                    placeholder="0"
+                                    min={0}
+                                    onIonChange={handleNumberOfItemsChange}
+                                ></IonInput>
+                            </IonItem>
+
+                            <IonItem>
+                                <IonInput
+                                    label="Price of Item"
+                                    type="number"
+                                    placeholder="0"
+                                    min={0}
+                                    onIonChange={handlePriceOfItemChange}
+                                ></IonInput>
+                            </IonItem>
+
+                            <IonItem>
+                                <IonInput
+                                    label="Total"
+                                    type="number"
+                                    placeholder="0"
+                                    min={0}
+                                    value={total}
+                                    readonly
+                                ></IonInput>
+                            </IonItem>
+
+                        </IonList>
+                    </IonRow>
+                </IonGrid>
+            </IonContent>
+        </IonPage>
+    );
+};
+
+export default ManualEntry;
