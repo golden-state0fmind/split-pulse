970dac200c2e675ec8143a92ccd95d76b3dd8f2a
golden-state0fmind <areyes2009@yahoo.com>
2024-03-13 23:12:48 -0700

    updates the dev service worker

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.bvj0483hg3"
+    "revision": "0.rrur7kei1f"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
