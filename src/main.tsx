bb6838deccb66e1c2032c99f094378bfc5593ab4
golden-state0fmind <areyes2009@yahoo.com>
2024-03-19 23:40:19 -0700

    adds redux store provider to app

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import { defineCustomElements } from '@ionic/pwa-elements/loader';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
 
 defineCustomElements(window);
 const container = document.getElementById('root');
 const root = createRoot(container!);
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
